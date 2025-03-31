cabec6c5644a46faeb7b80cbe6f9af7c06e945e3
DESKTOP-VEH7IM7\Administrator <410510921@qq.com>
2025-03-31 19:09:55 +0800

    初始化仓库

---
+++ Source/QiDian/QiDianGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "QiDianGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AQiDianGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AQiDianGameMode();
+};
+
+
+
